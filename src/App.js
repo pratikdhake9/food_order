6652582840dd0b6b415bc1d56bb6b7d8c7730019
Pratik Dhake <83688851+pratikdhake9@users.noreply.github.com>
2025-09-11 19:25:04 +0530

    set...

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h1 className={""}>Hello world</h1>
+        <h1 className={'text-5xl'}>Hello world</h1>
     </div>
   );
 }
